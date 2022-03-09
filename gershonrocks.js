134061a4298707926d60e678b386b37d07fd6251
Gershon <CTepper625@gmail.com>
2022-03-09 12:26:01 +0200

    added gershonrocks.js,multiplybob.js

---
+++ gershonrocks.js
@@ -0,0 +1,4 @@
+let name = "Gershon";
+let rocks = "rocks";
+let exclaim = "!!!!!";
+alert(name + rocks + exclaim);
---
+++ multiplybob.js
@@ -0,0 +1,6 @@
+let name = "bob";
+let number = 5;
+let exclaim = "!";
+alert(name * number + exclaim);
+exclaim = "!!!"
+alert(name * number + exclaim);
