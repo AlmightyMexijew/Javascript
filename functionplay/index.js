39d9323f1d2022fafdefcfcc516433c4278f847c
Gershon <CTepper625@gmail.com>
2022-05-05 11:23:54 +0300

    new work for rockpaperscissors

--- functionplay/index.js
+++ functionplay/index.js
@@ -6,3 +6,8 @@ function multiply(number,number2){
     const result = number * number2;
     return result;
 }
+
+function capitalize(mystring){
+    let mystring1 = mystring;
+    console.log(mystring1)
+}
