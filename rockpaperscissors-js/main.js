da040389c943123a5a461c1cb643a23f83bdb34d
almightymexijew <ctepper625@gmail.com>
2023-04-27 09:56:24 +0300

    updates and new tries

---
+++ rockpaperscissors-js/main.js
@@ -0,0 +1,11 @@
+//player clicks button rock,paper,scissors
+let playerRock = document.querySelector('#rock').addEventListener('click')
+let playerPaper = document.querySelector('#paper').addEventListener('click')
+let playerScissors = document.querySelector('#scissors').addEventListener('click')
+//player choice assigned to variable playerChoice
+
+//computer choice assigned to variable aiChoice
+//computer choice should be randomized
+//make a comparison afterward between playerChoice and aiChoice-- possibly using an assignment system that gives each one a variable with number assignment.
+//output to console and to document
+// ** idea**  assign player a value/variable/something, ai another, use if statements to compare and decide outcome for each case.
---
+++ tictactoegamejs/main.js
@@ -0,0 +1,9 @@
+//player clicks button rock,paper,scissors
+document.querySelector('rock').addEventListener('click',playerChoiceFunc);
+//player choice assigned to variable playerChoice
+let playerChoice
+//computer choice assigned to variable aiChoice
+//computer choice should be randomized
+//make a comparison afterward between playerChoice and aiChoice-- possibly using an assignment system that gives each one a variable with number assignment.
+//output to console and to document
+// ** idea**  assign player a value/variable/something, ai another, use if statements to compare and decide outcome for each case.
